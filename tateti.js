e3a56f95f1b2c62ef9aaeebfe9a7f3932d78a93e
Daiana Panqueca <dai@MacBook-Pro-3.local>
2022-04-02 10:00:11 -0300

    clean up

--- tateti.js
+++ tateti.js
@@ -40,54 +40,17 @@ let tie = (string) => {
 }
 
 let render = (boardGame,turn) => {
-    // objetivo de esta funcion : muestra la informacion del tablero a los jugadores. necesita acceder al tablero y a turno
-
-   //aca lo que hago es agarrar cada casillero de la tabla que hice en el archivo html y asignarle cada valor que haya guardado en el array que representa al tablero.
-    //ahora el array esta vacio,pero la idea es que sea el lugar donde se almacene la informacion de cada jugada. 
-
-    //que es lo que estoy modificando?
-        //el array boardGame
-    //podria recorrer el array con un for
-    //ahora bien, como capturo cada elemento,cada casillero,a fin de poder asignarle el valor que tiene adentro cada casillero?
-        //que tienen en comun? la clase y el tag
-        //getelementbytagname devuelve una lista
-    //tengo dos listas que recorrer y es asignar``````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````''''''''''============================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================
+    // objetivo de esta funcion : muestra la informacion del tablero a los jugadores. necesita acceder al tablero y a turno   
         let squares = document.getElementsByClassName('board')
 
-/* 
-        Array.from(squares).forEach(function (element) {
-            console.log(element)
-          }); */
         for(let i = 0;i < squares.length;i++){
           squares[i].textContent = boardGame[i];
         }; 
-   
-
-   /* document.getElementById('1').textContent = boardGame[0];
-   document.getElementById('2').textContent = boardGame[1];
-   document.getElementById('3').textContent = boardGame[2];
-   document.getElementById('4').textContent = boardGame[3];
-   document.getElementById('5').textContent = boardGame[4];
-   document.getElementById('6').textContent = boardGame[5];
-   document.getElementById('7').textContent = boardGame[6];
-   document.getElementById('8').textContent = boardGame[7];
-   document.getElementById('9').textContent = boardGame[8]; */
-   
-    //todavia no estoy muy segura con respecto a turno por lo que esta sucediendo en la funcion onclick.pienso que deberia poder acceder a turno para renderizar, pero en onclick estoy mostrando algo al usuario.
    // aca deberia poder mostrar a quien le toca jugar:
 
     let turnElement = document.getElementById('turn').textContent = `Juega: ${turn}`;
     
     }
-   
-
-
-
-//funcionalidad a implementar : mostrar en el HTML a quien le toca jugar
-//0)armar el html con el que se va a mostrar a quien le toca
-
-//1)almacenamiento y flujo de a quien a le toca  (hacer que el valor actual del turno le llegue a render())
-//2)mostrar esa informacion almacenada en el html 
 
 
 function onClick (turn,boardGame) {
@@ -96,8 +59,8 @@ function onClick (turn,boardGame) {
     //si me hacen click, depende quien juega,quiero mostrar x o circulo 
 
     
-   var primerCasillero = document.getElementById('1')  ; //obtengo el elemento con id '1' de mi archivo html y lo guardo en variable.                                                                                                     
-   primerCasillero. addEventListener('click',() => { // aca le agrego un detector de eventos, donde digo que cuando le hagan click a ese elemento suceda lo siguiente:
+   var primerCasillero = document.getElementById('1')  ; 
+   primerCasillero. addEventListener('click',() => { 
 
         boardGame[0] = turn;
         
@@ -108,31 +71,6 @@ function onClick (turn,boardGame) {
         }
         
         render(boardGame,turn);
-
-        
-
-
-
-
-    // version vieja
-
-
-
-        /*if(turn == 'x'){ //en caso de que el turno sea de 'x'
-            primerCasillero.innerHTML = turn; // a ese casillero le asigno el valor de turno para que muestre 'x'
-            window.alert('juega circulo'); // tiro un alert que indica a quien le toca ahora
-            turn = 'o'; //cambio el valor de turno para que ahora sea circulo 'o'
-            boardGame[0] = primerCasillero.innerHTML; //aca al primer casillero del tablero le asigno al casillero lo que contiene el casillero. estoy dudando un poco de este paso, xq siento que estoy pisando info del tablero.
-            
-            render(boardGame)
-            // llamo a render para que muestre la info del tablero. me parece que no esta funcionando igual, o no es
-        }
-        else{
-            primerCasillero.innerHTML = turn; // si el turno no es de 'x'
-            window.alert('juega x');
-            turn = 'x';
-            boardGame[0] = primerCasillero.innerHTML;
-        }*/
     }) 
 
     
