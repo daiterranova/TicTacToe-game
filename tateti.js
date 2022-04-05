9fe7af8a7965978056081eb33f557b09a8df1272
Daiana Panqueca <dai@Daianas-MacBook-Air.local>
2022-04-05 16:30:26 -0300

    cambios en onclick

--- tateti.js
+++ tateti.js
@@ -1,8 +1,11 @@
 //que necesito ?
 // un tablero
-let boardGame = new Array(9);
 
-boardGame = [];
+
+let boardGame = new Array(9);// aca esta el estado interno del tablero, es la informacion que tiene adentro el tablero (parte logica)
+let turn = 'x';
+let winner = null;
+let squares = document.getElementsByClassName('board');// estos son los casilleros de html(interfaz grafica,representacion grafica,lo que se muestra)
 
 
 
@@ -11,74 +14,97 @@ cada casillero puede adquirir uno de 3 valores(jugo x,jugo o o no jugado)
 despues de cada jugada chequear si gano alguien y si hubo empate
 guardar a que jugador le toca jugar */
 
+//bloque whoWins
 
 
 
-let turn = 'x';
+let whoWins = (winner) =>{
 
-let squares = document.getElementsByClassName('board');
-
-let whoWins = () =>{
 //chequea si alguien gano, necesita acceder a la informacion guardada en el tablero y saber de quien era el turno
 //cuando se gana?
 // cuando uno de los dos jugadores completa la combinacion
 // 0-2,3-5,6-8 o 0,4,8 o 2,4,6
     
-   /*  if(boardGame[0] == 'x' && boardGame[1] =='x' && boardGame[2] == 'x' || boardGame[3] == && boardGame[4] && boardGame[5] || boardGame[6] && boardGame[7] && boardGame[8] || boardGame[0] && boardGame[4] && boardGame[8] || boardGame[2] && boardGame[4] && boardGame[6] == 'x' ){
-        
-       console.log('gana x')  
+    if(boardGame[0] == 'x' && boardGame[1] =='x' && boardGame[2] == 'x' || boardGame[3] == 'x' && boardGame[4] == 'x' && boardGame[5] =='x' || boardGame[6] =='x' && boardGame[7] == 'x' && boardGame[8] =='x' || boardGame[0] =='x' && boardGame[4]=='x' && boardGame[8] =='x' || boardGame[2] == 'x' && boardGame[4] =='x' && boardGame[6] == 'x' ){
+       
+        winner = 'x';
+       
     }
-    else if(boardGame[0] && boardGame[1] && boardGame[2] || boardGame[3] && boardGame[4] && boardGame[5] || boardGame[6] && boardGame[7] && boardGame[8] || boardGame[0] && boardGame[4] && boardGame[8] || boardGame[2] && boardGame[4] && boardGame[6]=== 'o' ){
-       console.log('gana o')  
+    else if(boardGame[0] == 'o' && boardGame[1]  == 'o' && boardGame[2] == 'o'  || boardGame[3] == 'o'  && boardGame[4] == 'o'  && boardGame[5] == 'o'  || boardGame[6] == 'o'  && boardGame[7] == 'o' && boardGame[8] == 'o' || boardGame[0] == 'o'  && boardGame[4]== 'o'  && boardGame[8] == 'o' || boardGame[2] == 'o'  && boardGame[4] == 'o'  && boardGame[6]=== 'o' ){
+       
+       winner ='o'; 
+      
     }
-    else{} */
+    
 }
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//bloque tie
 
 let tie = (string) => {
     //tiene que chequear si nadie gano y no hay mas jugadas posibles. necesita acceder a la info del tablero y de quien era el turno 
-    console.log(`no mas jugadas,${string}`)
+    
 }
 
-let render = (boardGame,turn) => {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+//bloque render
+
+// render deberia ser el unico encargado de interactuar con el html , en este caso es el unico que le va a pasar que mostrar a squares.
+
+let render = (boardGame,turn,winner) => {
     // objetivo de esta funcion : muestra la informacion del tablero a los jugadores. necesita acceder al tablero y a turno   
+    
         for(let i = 0;i < squares.length;i++){
-          squares[i].textContent = boardGame[i];
+          squares[i].textContent = boardGame[i]; // la propiedad text content va a ser el valor del tablero en ese lugar.
         }; 
     
         let turnElement = document.getElementById('turn').textContent = `Juega: ${turn}`;
-    
+        let winnerElement = document.getElementById('winner').textContent = `Ganador: ${winner}`;
     }
 
-  
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//bloque onclick
+
+
 function onClick (turn,boardGame) {
+ // manejar la logica sin interactuar directamente con el html(squares)
+
     //tiene que detectar donde se hizo click en el tablero
     // y mostrar el paso siguiente,
     //si me hacen click, depende quien juega,quiero mostrar x o circulo 
-    
+  
   for (let i = 0; i < squares.length; i++) {
       //queremos que cuando se haga click en tal casillero, si ese casillero esta vacio, asigne 'x' u 'o' dependiendo de quien sea el turno.
         squares[i].addEventListener('click',() => { 
-                if (squares[i].textContent == "") {
-                boardGame[i] = turn;
+                if (boardGame[i] == null) {                  
+                    boardGame[i] = turn;
                 if (turn == "x") {
                     turn = "o";
                 } else {
                     turn = "x";
                 }
+                console.log(boardGame[i])
+                console.log(boardGame)
                 }
-                render(boardGame,turn);
+                whoWins(winner);
+                tie();
+                render(boardGame,turn,winner);
             }) 
         
-        } 
-    // llamar a las funciones
-        // ganar 
-        //todavia no estan bien implementadas, solo queria ver si funcionaban
-        whoWins()
-        //empate  
-        tie('empate')
-  
+        }
  }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 onClick(turn,boardGame)
-render(boardGame,turn)
+
